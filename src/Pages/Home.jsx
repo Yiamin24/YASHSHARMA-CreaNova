a97f7a188dd92861cd3d62e4422c63d1aef7790a
Yiamin24 <yashsharma02412@gmail.com>
2025-10-06 22:19:24 +0530

    home

--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -18,7 +18,7 @@ export default function Home() {
       <AboutMeTitle/>
       <AboutSection/>
       <WorksTitle/>
-      <Work/>
+      <Work/> 
       <ServicesTitle/>
       <Services/>
       <Connect/>
